6fa3d1802a57a255a0757ef4ddd68dd5c985a125
Giovani Silva <giovani.m.silva@gmail.com>
2018-05-09 10:17:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var oracledb = require('oracledb');
+require('dotenv').config();
+
+//Criar arquivo .env com as configs
+//DB_USER=user
+//DB_PASS=pass
+//DB_HOST=host:post/instancia
+
+oracledb.getConnection(
+    {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        connectString: process.env.DB_HOST
+    },
+    function (err, connection) {
+        if (err) { console.error(err); return; }
+        connection.execute(
+            "SELECT count(1) from AHX_AIRCRAFT",
+            function (err, result) {
+                if (err) { console.error(err); return; }
+                console.log(result.rows);
+            });
+    });
